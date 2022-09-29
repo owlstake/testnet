76eeac2dab97b3fffa6496e1050beb875bb00426
owlstake.com <107361397+owlstake@users.noreply.github.com>
2022-09-30 01:13:20 +0700

    Update subspace-auto-install.sh

--- subspace/subspace-auto-install.sh
+++ subspace/subspace-auto-install.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+########################
+# install docker
+sudo apt update && sudo apt upgrade -y
+sudo apt install curl build-essential git wget jq make gcc ack tmux ncdu -y
+sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.27.3/yq_linux_amd64 && chmod +x /usr/local/bin/yq
+apt update && apt install git sudo unzip wget -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+########################
 counter=0
 CPUCORE=$(nproc)
 readarray -t arrrewardaddress <rewardaddress.txt 2> /dev/null
@@ -14,8 +26,8 @@ PORTARRAY=()
 #Ask port ranges array
 function ADDPORTARRAY {
 PORTARRAY+=(4000)
-PORTARRAY+=(5000)
-PORTARRAY+=(6000)
+PORTARRAY+=(4008)
+PORTARRAY+=(4016)
 }
 ##########################
 function install {
